5b7260e578be85f4dd092f5295581c0227904c46
liudan <ld17535185284@163.com>
2020-11-05 16:25:56 +0800

    Manager Work1 Commit

--- src/main/java/com/atguigu/git/dan1216/Work1.java
+++ src/main/java/com/atguigu/git/dan1216/Work1.java
@@ -3,5 +3,6 @@ package com.atguigu.git.dan1216;
 public class Work1 {
     public static void main(String[] args) {
         System.out.println("自己敲代码！！！");
+        System.out.println("一个人更精彩~~~");
     }
 }
